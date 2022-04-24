7c4c29253292a1782348480ff14e44d931c65fa7
Mohamed Samir <mosamircs@gmail.com>
2022-04-24 04:05:11 +0200

    solved empty register data

--- register.php
+++ register.php
@@ -6,40 +6,40 @@ if(isset($_GET["id"])){
 }
 $database_instance = Database::getInstance();
 $connection = $database_instance->getConnection(); 
-global $formdata;
-$formdata = array();
-if (isset($_POST["username"])) {
-    $formdata["username"] = $_POST["username"];
-}
-if (isset($_POST["email"])) {
-    $formdata["email"] = $_POST["email"];
-    $sql = "SELECT * FROM users where email='".$formdata["email"]."'";
-    $result = $connection->query($sql);
-    $num_rows = mysqli_num_rows($result);
-    if($num_rows >= 1){
-        // echo "email exist";
-    }else{
-        $email = $formdata["email"];
-            $sql = "SELECT * FROM users where email='".$formdata["email"]."'";
-            $result = $connection->query($sql);
-    }
-}
-if (isset($_POST["phone"])) {
-    $formdata["phone"] = $_POST["phone"];
-    $sql = "SELECT * FROM users where phone='".$formdata["phone"]."'";
-    $result = $connection->query($sql);
-    $num_rows = mysqli_num_rows($result);
-    if($num_rows >= 1){
-        // echo "email exist";
-    }else{
-        $phone = $formdata["phone"];
-        $sql = "SELECT * FROM users where phone='".$formdata["phone"]."'";
-        $result = $connection->query($sql);
-    }
-}  
-if (isset($formdata["username"])&&isset($formdata["email"])&&isset($formdata["phone"])) {
+// global $formdata;
+// $formdata = array();
+// if (isset($_POST["username"])) {
+//     $formdata["username"] = $_POST["username"];
+// }
+// if (isset($_POST["email"])) {
+//     $formdata["email"] = $_POST["email"];
+//     $sql = "SELECT * FROM users where email='".$formdata["email"]."'";
+//     $result = $connection->query($sql);
+//     $num_rows = mysqli_num_rows($result);
+//     if($num_rows >= 1){
+//         // echo "email exist";
+//     }else{
+//         $email = $formdata["email"];
+//             $sql = "SELECT * FROM users where email='".$formdata["email"]."'";
+//             $result = $connection->query($sql);
+//     }
+// }
+// if (isset($_POST["phone"])) {
+//     $formdata["phone"] = $_POST["phone"];
+//     $sql = "SELECT * FROM users where phone='".$formdata["phone"]."'";
+//     $result = $connection->query($sql);
+//     $num_rows = mysqli_num_rows($result);
+//     if($num_rows >= 1){
+//         // echo "email exist";
+//     }else{
+//         $phone = $formdata["phone"];
+//         $sql = "SELECT * FROM users where phone='".$formdata["phone"]."'";
+//         $result = $connection->query($sql);
+//     }
+// }  
+if (isset($_POST["username"])&&isset($_POST["email"])&&isset($_POST["phone"]) && !empty($_POST["phone"])&& !empty($_POST["username"])&&!empty($_POST["email"])) {
     $insert_user = "INSERT INTO `users` (`name`, `email`, `phone`) 
-VALUES ('".$formdata["username"]."','".$formdata["email"]."', '".$formdata["phone"]."')";
+VALUES ('".$_POST["username"]."','".$_POST["email"]."', '".$_POST["phone"]."')";
     $result = $connection->query($insert_user);
 
     $last_id = $connection->insert_id;
